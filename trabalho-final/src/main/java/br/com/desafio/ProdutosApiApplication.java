470e0981e28cb373d2240c52daef67c63b0daf4d
eduardoajs2020 <eduardoajs2020@gmail.com>
2026-07-24 17:05:30 -0300

    v2

---
+++ trabalho-final/src/main/java/br/com/desafio/ProdutosApiApplication.java
@@ -0,0 +1,19 @@
+﻿
+package br.com.desafio;
+ 
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+
+public class ProdutosApiApplication {
+ 
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProdutosApiApplication.class, args);
+
+    }
+
+}
+
---
+++ trabalho-final/src/main/java/br/com/desafio/model/Produto.java
@@ -0,0 +1,65 @@
+﻿
+package br.com.desafio.model;
+ 
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Table;
+ 
+@Entity
+
+@Table(name = "produtos")
+
+public class Produto {
+ 
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+ 
+    private String nome;
+ 
+    public Produto() {
+
+    }
+ 
+    public Produto(Long id, String nome) {
+
+        this.id = id;
+
+        this.nome = nome;
+
+    }
+ 
+    public Long getId() {
+
+        return id;
+
+    }
+ 
+    public String getNome() {
+
+        return nome;
+
+    }
+ 
+    public void setId(Long id) {
+
+        this.id = id;
+
+    }
+ 
+    public void setNome(String nome) {
+
+        this.nome = nome;
+
+    }
+
+}
+
